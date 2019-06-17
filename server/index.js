64c9850eb096c398893b5335082028deb482e7d4
david <davidwnewman91@gmail.com>
2019-06-17 11:06:39 -0500

    tab named, database connected

---
+++ server/index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const app = express();
+const session = require('express-session');
+const massive = require('massive');
+require('dotenv').config();
+
+// controller imports
+
+
+const {SERVER_PORT, CONNECTION_STRING, SESSION_SECRET} = process.env
+
+app.use(express.json())
+app.use(session({
+    secret: SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        maxAge: 1000*60*60*24*7
+    }}))
+massive(CONNECTION_STRING)
+    .then(db =>{
+        console.log('Database connection successful!')
+        app.set('db', db)
+    })
+    .catch(()=>{console.log('Database connection failed...')})
+
+// endpoints
+
+
+app.listen(SERVER_PORT, ()=> {
+    console.log(`Listening on port ${SERVER_PORT}.`)
+})
