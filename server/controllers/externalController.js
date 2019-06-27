34872e6f3e551bc7c8a9e1b3a73076d12ee83320
david <davidwnewman91@gmail.com>
2019-06-27 09:52:31 -0500

    externalController added for Stripe and Nodemailer

---
+++ server/controllers/externalController.js
@@ -0,0 +1,60 @@
+require('dotenv').config();
+
+const {STRIPE_SECRET_KEY} = process.env
+
+// Stripe (payment)
+const stripe = require('stripe')(STRIPE_SECRET_KEY);
+const uuid = require('uuid/v4');
+
+async function checkout(req, res) {
+    // console.log("Request:", req.body);
+    // console.log('stripe: ', stripe.customers)
+    // console.log(stripe.charges)
+    
+    let error;
+    let status;
+    try {
+        const { total, cart, token } = req.body;
+        console.log('Cart: ', cart)
+    
+        const customer = await stripe.customers.create({
+        email: token.email,
+        source: token.id
+        });
+    
+        const idempotency_key = uuid();
+        const charge = await stripe.charges.create({
+            amount: (Math.round(total * 100)),
+            currency: "usd",
+            customer: customer.id,
+            receipt_email: token.email,
+            description: `Purchased the ${[items]}`,
+            shipping: {
+                name: token.card.name,
+                address: {
+                    line1: token.card.address_line1,
+                    line2: token.card.address_line2,
+                    city: token.card.address_city,
+                    country: token.card.address_country,
+                    postal_code: token.card.address_zip
+                }
+            }
+        },
+        {
+            idempotency_key
+        }
+        );
+        console.log("Charge:", { charge });
+        status = "success";
+    } catch (error) {
+        console.error("Error:", error);
+        status = "failure";
+    }
+    
+    res.json({ error, status });
+    console.log(status)
+};
+
+module.exports ={
+    checkout
+}
--- server/index.js
+++ server/index.js
@@ -10,6 +10,7 @@ const ac = require('./controllers/authController');
 const pc = require('./controllers/productController');
 const dc = require('./controllers/designerController');
 const auth = require('./middleware/auth.Middleware');
+const ec = require('./controllers/externalController');
 
 
 // general server setup w/session and database
@@ -50,68 +51,14 @@ app.post('/api/order', auth.usersOnly, pc.placeOrder)
 app.put('/api/cart/:id')
 app.delete('/api/cart/:id', auth.usersOnly, pc.removeCart)
 
+// --externalController--
+app.post('/api/checkout',auth.usersOnly, ec.checkout)
+
 //--designerController-- 
 app.get('/api/orders', auth.adminsOnly, dc.getOrder)
 app.post('/api/new_item', auth.adminsOnly, dc.addProduct)
 app.put('/api/inventory/:product_id', auth.adminsOnly, dc.editProduct)
 app.delete('/api/inventory/:product_id', auth.adminsOnly, dc.removeProduct)
-
-// Stripe (payment)
-const stripe = require('stripe')('sk_test_r3kahHBHQM7gx961ULee5Rpb00wGAs8kQB');
-const uuid = require('uuid/v4');
-// const cors = require('cors')
-
-// app.use(cors());
-
-app.post("/api/checkout", async (req, res) => {
-    // console.log("Request:", req.body);
-    // console.log('stripe: ', stripe.customers)
-    // console.log(stripe.charges)
-    
-    let error;
-    let status;
-    try {
-        const { total, cart, token } = req.body;
-        console.log('Cart: ', cart)
-    
-        const customer = await stripe.customers.create({
-        email: token.email,
-        source: token.id
-        });
-    
-        const idempotency_key = uuid();
-        const charge = await stripe.charges.create(
-        {
-            amount: (Math.round(total * 100)),
-            currency: "usd",
-            customer: customer.id,
-            receipt_email: token.email,
-            description: `Purchased the ${[items]}`,
-            shipping: {
-            name: token.card.name,
-            address: {
-                line1: token.card.address_line1,
-                line2: token.card.address_line2,
-                city: token.card.address_city,
-                country: token.card.address_country,
-                postal_code: token.card.address_zip
-            }
-            }
-        },
-        {
-            idempotency_key
-        }
-        );
-        console.log("Charge:", { charge });
-        status = "success";
-    } catch (error) {
-        console.error("Error:", error);
-        status = "failure";
-    }
-    
-    res.json({ error, status });
-    console.log(status)
-    });
 
 // DEFAULT
     app.listen(SERVER_PORT, ()=> {
